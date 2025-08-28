a171b3e7007a1eadae8079dae440b5b8683f86dc
Gilocah28 <gberxx@gmail.com>
2025-08-29 00:16:02 +0800

    fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,15 @@ import { products } from "../../starting-code/data/products";
 import Header from "../components/Header";
 
 const HomePage = () => {
+  // fetch('http://localhost:3000/api/products')
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce project</title>
