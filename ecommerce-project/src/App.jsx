99c77a69bfbde1a1420cb7dfcedeac1cb3477e22
Gilocah28 <gberxx@gmail.com>
2025-08-28 21:19:23 +0800

    hearder link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
+import OrderPage from "./pages/OrderPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrderPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
