6b6765975ef79cde0a700bf576bdbc27ed975801
Gilocah28 <gberxx@gmail.com>
2025-09-07 16:31:32 +0800

    test clicking to add to cart button

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -20,7 +20,11 @@ export const Product = ({ product, loadCart }) => {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} data-testid="product-image"/>
+        <img
+          className="product-image"
+          src={product.image}
+          data-testid="product-image"
+        />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -60,7 +64,11 @@ export const Product = ({ product, loadCart }) => {
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button
+        className="add-to-cart-button button-primary"
+        data-testid="add-to-cart-button"
+        onClick={addToCart}
+      >
         Add to Cart
       </button>
     </div>
